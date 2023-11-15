feec12f73c30fba37d14a3243a4737139b8ae415
Pavan212k <shilratna.chawhan@skillassure.com>
2023-11-15 16:24:38 +0530

    Portfolio Website Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
+import { Title, Meta } from '@angular/platform-browser';
+import { LanguageService } from "src/app/services/language/language.service"
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'shilratna-portfolio';
+
+  constructor(
+    private titleService: Title,
+    private metaService: Meta,
+    private languageService: LanguageService
+  ) {
+  }
+  ngOnInit(): void {
+
+    this.languageService.initLanguage()
+
+    this.titleService.setTitle("Shilratna Chawhan | Software Engineer");
+ 
+    AOS.init();
+
+  }
+}
